bbaf6634597ddfb14a0422e953adc791172b6cc7
tehskai <skailain@gmail.com>
2016-12-24 21:08:07 +0700

    first working version

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # godot-unpacker
-godot .pck unpacker
+godot .pck unpacker for non-encrypted files
+## system requirements
+* php 7.0 64bit
+## usage
+Move data.pck in the same directory as script and run:
+```
+php -f godot-unpacker.php
+```
+## todo
+* uncompress compressed files in process
+* fix filename encoding on windows machines
+* rewrite into python
--- godot-unpacker.php
+++ godot-unpacker.php
@@ -1,41 +1,69 @@
 <pre>
-<?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+<?php 
+/*
+  godot-unpacker.php (https://github.com/tehskai/godot-unpacker)
+  tool for extracting game files from godot engine data.pck binary packs (UNENCRYPTED)
+  really just a proof-of-concept, tested on php 7.0.1 62bit (windows7 64bit)
+*/
 
-$f = fopen('./data.pck', 'rb');
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+set_time_limit(0);
+
+$file = "data.pck";
+$f = fopen("./${file}", "rb");
 
 $bin_file_header = fread($f, 20+64+4);
 
-$file_header_format = implode('/', [
-  'Lmagic',
-  'Lversion',
-  'Lver_major',
-  'Lver_minor',
-  'Lver_rev',
-  //'L16reserved',
-  'x64reserved',
-  'Ifile_count'
+$file_header_format = implode("/", [
+  "Lmagic",
+  "Lversion",
+  "Lver_major",
+  "Lver_minor",
+  "Lver_rev",
+  //"L16reserved",
+  "x64reserved", // reserved null-bytes
+  "Ifile_count" // total file count
 ]);
 
 $file_header = unpack($file_header_format, $bin_file_header);
+
 $packed_files = Array();
 
-for ($i = 0; $i < $file_header['file_count']; $i++) {
+echo "File {$file} (Godot engine v{$file_header['ver_major']}.{$file_header['ver_minor']}), total of {$file_header['file_count']}:".PHP_EOL;
+
+for ($i = 0; $i < $file_header["file_count"]; $i++) {
   $sl = fread($f, 4);
-  $sl = unpack('Lsl', $sl);
-  $sl = $sl['sl'];
+  $sl = unpack("Lsl", $sl);
+  $sl = $sl["sl"]; // path length
   $file_info = fread($f, $sl+8+8+16);
-  $packed_files[] = unpack('A'.$sl.'data/Qoffset/Qsize/H32md5', $file_info);
+  $packed_file_info = unpack("A{$sl}path/Qoffset/Qsize/H32md5", $file_info); // php 7 64bit
+  $packed_files[] = $packed_file_info;
+  // $packed_file = unpack("A${sl}path/Loffset/Lsize/H32md5", $file_info);  // php 5.5
+  echo "{$packed_file_info['md5']} {$packed_file_info['path']} ({$packed_file_info['size']} bytes)".PHP_EOL;
 }
 
-// fseek($f, 1685795);
-// $data = fread($f, 2392);
-// $u = fopen('ui.gdc', 'wb');
-// fputs($u, $data);
-// fclose($u);
+for ($i = 0; $i < count($packed_files); $i++) {
+  $offset = $packed_files[$i]['offset'];
+  $size = $packed_files[$i]['size'];
+  $complete_path = str_replace("res://", "", $packed_files[$i]['path']);
+  $relative_path = dirname($complete_path);
+  $filename = basename($complete_path);
+
+  if (!file_exists($relative_path)) {
+      mkdir($relative_path, 0777, true);
+      echo "Directory {$relative_path} doesn't exist. Creating.".PHP_EOL;
+  }
+
+  fseek($f, $offset);
+  $data = fread($f, $size);
+
+  $u = fopen($complete_path, "wb");
+  fputs($u, $data);
+  fclose($u);
+
+  echo "Extracting {$filename} from {$offset} / {$size} bytes / to {$complete_path} / {$relative_path}".PHP_EOL;
+}
 
 fclose($f);
-print_r($file_header);
-print_r($packed_files);
 ?>
